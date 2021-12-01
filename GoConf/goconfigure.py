4bbbee1dd10c7c7df53640d85a33d16c6908fb94
lewis <lewis.morris@gmail.com>
2021-12-01 10:22:44 +0000

    init

--- GoConf/goconfigure.py
+++ GoConf/goconfigure.py
@@ -9,10 +9,15 @@ warnings.filterwarnings("ignore")
 
 class GoConfigure:
     def __init__(self, path):
+        """
+        :param path: should be a valid file path. It does not hav currently exist. If the file does not exist then it
+        will be created.
+        """
         self._fl = path
         self._conf_dict = OrderedDict({})
         if not os.path.isfile(path):
             self._create_conf()
+        self._read_fl_into()
 
     def __getitem__(self, key):
         return self._conf_dict[key]
@@ -44,6 +49,8 @@ class GoConfigure:
         :return:
         """
         self._conf_dict = OrderedDict({})
+        current_section = None
+
         with open(self._fl, "r") as f:
             for line in f:
                 ln = line.strip()
@@ -62,8 +69,11 @@ class GoConfigure:
                     values = [x.strip() for x in val.group(0).split("=")]
                     try:
                         self._conf_dict[current_section][values[0]] = ast.literal_eval(values[1])
-                    except ValueError:
-                        self._conf_dict[current_section][values[0]] = values[1]
+                    except (ValueError,SyntaxError):
+                        self._conf_dict[current_section][values[0]] = check_bool(values[1])
+    def __check_section(self, current_section):
+        if current_section is None:
+            raise Exception("You have not defined a main block. This should appear like [SECTION] ")
 
     def write(self):
         """
@@ -82,5 +92,11 @@ class GoConfigure:
         with open(self._fl, "w") as f:
             f.write(text)
 
-
-
+def check_bool(string):
+    string = string.strip()
+    if string in ["true","True"]:
+        return True
+    elif string in ["false","False"]:
+        return False
+    else:
+        return string
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='goconfigure',
+    version='0.01',
+    packages=['GoConf'],
+    url='https://github.com/lewis-morris/GoConfigure',
+    license='MIT',
+    author='Lewis Morris',
+    author_email='lewis.morris@gmail.com',
+    description='Basic python config parser.'
+)
